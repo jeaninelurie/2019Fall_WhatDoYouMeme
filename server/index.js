b1016367eeb3e193943d8dc2efddc215a9ba98db
jeaninelurie <jplurie98@gmail.com>
2019-10-03 12:22:57 -0400

    Rest API

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const userController = require('./controllers/Users');
+
 const app = express();
 const port = 3000;
 
 app
+    .use(function( req, res, next) {
+        // Logging
+        console.log({ params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers });
+        next();
+    })
     .use('/static', express.static(path.join(__dirname, '../NoFramework')))
 
     .get(   '/', 
@@ -11,8 +18,9 @@ app
         )
     .get(   '/heb', 
         function(req, res){
-             res.send('Shalom World!')
+             res.send({ msg: 'Shalom World!'})
         }
-    );
+    )
+    .use('/users', userController);
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
